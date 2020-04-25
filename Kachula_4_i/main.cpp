54d7837d2be44267204ac1d351901ab74197525e
sampletext32 <sampletext32@bk.ru>
2020-04-25 06:39:58 +0300

    Add project

---
+++ Kachula_4_i/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+
+	cout << "Программу сделал Качула Михаил Викторович студент группы УМЛ-112" << "\n";
+	cout << "Программа \"вычисление таблицы функции\"" << endl;
+
+	for (double x = 0; x <= 1.2; x += 0.05)
+	{
+		double result = 0;
+		double a = 1;
+		for (int i = 0; i < 20; i++)
+		{
+			result += (i + 1) * a;
+			a *= x;
+		}
+		cout << "y(" << x << ") = " << result << endl;
+	}
+
+	return 0;
+}
